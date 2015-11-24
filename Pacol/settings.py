8e1f482833ff5815b88b4746c2626aa48f243154
ad@ad.co <ad@ad.co>
2015-11-24 00:34:47 -0500

    fix deploy

--- Pacol/settings.py
+++ Pacol/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'gunicorn',
     'material', 'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
--- Pacol/wsgi.py
+++ Pacol/wsgi.py
@@ -14,5 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Pacol.settings")
 
 from dj_static import Cling
-
 application = Cling(get_wsgi_application())
